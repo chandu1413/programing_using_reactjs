9fe2d650df9d61cb4d7a49626fde885a0d827c86
chandu <blessychandu143@gmail.com>
2022-08-02 20:54:46 +0530

    rout

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import React from "react";
 import About from "./components/About";
 import Home from "./components/Home";
 import NavbarComp from "./components/NavbarComp";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter,Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
-  
-        <NavbarComp />
+    <BrowserRouter>
+    
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/About" element={<About/>}/>
+      </Routes>
+    </BrowserRouter>
       
   );
 }
