67dec63a66d2b088731b3e7dcc9d791897dc2d3b
chandu <blessychandu143@gmail.com>
2022-08-03 03:09:34 +0530

    card 2

--- src/components/card.js
+++ src/components/card.js
@@ -1,20 +1,37 @@
 import React from 'react'
 import img1 from '../images/c.png';
+import img2 from '../images/python.jpg';
+import img3 from '../images/java.png';
 
 const Card =(props)=>{
   return (
-    <div className='card text-center'>
-        <div className='overflow'>
-           <img src={img1} className='card-img-top' alt=''/> 
-        </div>
-        <div className='card-body text-dark'>
-            <h4 className='card-title'>card title</h4>
-            <p className='card-text text-secondary'>fjkdgdgkdgkgksgksg</p>
-            <a href='#' className='btn bt-outline-sucess'>
-                go 
-            </a>
-        </div>
+    <div className="card-group">
+    <div className="card">
+      <img src={img1} className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+      </div>
     </div>
+    <div className="card">
+      <img src={img2} className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+        <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+    <div className="card">
+      <img src={img3} className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+        <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+  
   )
 }
 
