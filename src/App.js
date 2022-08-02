8aaddfd9309351107e97e1a355d33dfbd1e8f93d
chandu <blessychandu143@gmail.com>
2022-08-02 20:48:15 +0530

    nav only

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
-import React from 'react';
-import { Link, Switch, Route } from 'react-router';
-import Nav from './nav';
-import Page1 from './page1';
-import Page2 from './page2';
-import Page3 from './page3';
+import React from "react";
 
-export default class App extends React.Component {
-  render() {    
-    return (
-      <div className="App">
-        <Router>
-          <div>
-            <Nav />
-            <Switch>
-              <Route exactly component={Landing} pattern="/" />
-              <Route exactly component={Page1} pattern="/path1" />
-              <Route exactly component={Page2} pattern="/path2" />
-              <Route exactly component={Page3} pattern="/path3" />
-              <Route component={Page404} />
-            </Switch>
-          </div>
-        </Router>
-      </div>
-    );
-  }
+import About from "./components/About";
+import Home from "./components/Home";
+import NavbarComp from "./components/NavbarComp";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+export default function App() {
+  return (
+  
+        <NavbarComp />
+      
+  );
 }
