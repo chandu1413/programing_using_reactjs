b238cf6ab7a7859d135910cda87f27e66c912908
chandu <blessychandu143@gmail.com>
2022-08-02 12:55:22 +0530

    start comit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Link, Switch, Route } from 'react-router';
+import Nav from './nav';
+import Page1 from './page1';
+import Page2 from './page2';
+import Page3 from './page3';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  render() {    
+    return (
+      <div className="App">
+        <Router>
+          <div>
+            <Nav />
+            <Switch>
+              <Route exactly component={Landing} pattern="/" />
+              <Route exactly component={Page1} pattern="/path1" />
+              <Route exactly component={Page2} pattern="/path2" />
+              <Route exactly component={Page3} pattern="/path3" />
+              <Route component={Page404} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    );
+  }
 }
-
-export default App;
