49ae303439437ab398dc3827ea12ca6171037958
chandu <blessychandu143@gmail.com>
2022-08-04 23:09:40 +0530

    ctest map

---
+++ src/components/Cdata.jsx
@@ -0,0 +1,40 @@
+import img1 from '../images/c.png';
+import img2 from '../images/java.png';
+import img3 from '../images/python.jpg';
+import img4 from '../images/js.png';
+import img5 from '../images/c++.png';
+
+
+const Sdata= [
+    {
+          img: {img1},
+          title: "c Language",
+          text: "C is a general-purpose programming language, developed in 1972, and still quite popular. C is very powerful; it has been used   to develop operating systems, databases, applications, etc.",
+        
+    },
+    {
+        img: {img2},
+        title: "java Language",
+        text :"Java is an object-oriented programming language that produces software for multiple platforms. When a programmer writes a Java application, the compiled code runs on most operating systems (OS), including Windows, Linux and Mac OS.",
+ 
+    },
+    {
+        img:{img3},
+          title:"python Language",
+          text:"Python is a high-level, interpreted, general-purpose programming language. Its design philosophy emphasizes code readability with the use of significant indentation.",
+    
+    },
+    {
+        img:{img4},
+        title:"Java script",
+      
+        text:"JavaScript is the world's most popular programming language.JavaScript is the programming language of the Web. JavaScript is easy to learn.",
+       
+    },
+    {
+        img:{img5},
+          title:"java Language",
+          text:"Jav  systems (OS), including Windows, Linux and Mac OS.",
+   
+    }
+]
